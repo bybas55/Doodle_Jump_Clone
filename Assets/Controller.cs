4598d378c08c8c89a0e8ca731b0d7dec98111111
Bayram Baş <bbas5534@hotmail.com>
2019-12-22 22:34:19 +0200

    project pertama

---
+++ Assets/Controller.cs
@@ -0,0 +1,83 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Controller : MonoBehaviour
+{
+
+    private Rigidbody2D rb2d;
+    private float moveInput;
+    private float speed = 10f;
+
+    private bool isStarted = false;
+
+    private float topScore = 0.0f;
+
+    public Text scoreText;
+    public Text startText;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+
+        rb2d = GetComponent<Rigidbody2D>();
+
+        rb2d.gravityScale = 0;
+        rb2d.velocity = Vector3.zero;
+
+    }
+
+     void Update()
+    {
+
+        if(Input.GetKeyDown(KeyCode.Space) && isStarted == false)
+        {
+
+            isStarted = true;
+            startText.gameObject.SetActive(false);
+            rb2d.gravityScale = 5f;
+
+        }
+
+        if (isStarted == true)
+        {
+
+            if (moveInput < 0)
+            {
+
+                this.GetComponent<SpriteRenderer>().flipX = false;
+
+            }
+            else
+            {
+
+                this.GetComponent<SpriteRenderer>().flipX = true;
+
+            }
+
+            if (rb2d.velocity.y > 0 && transform.position.y > topScore)
+            {
+
+                topScore = transform.position.y;
+
+            }
+
+            scoreText.text = "Score: " + Mathf.Round(topScore).ToString();
+        }
+
+    }
+
+    void FixedUpdate()
+    {
+
+        if (isStarted == true)
+        {
+
+            moveInput = Input.GetAxis("Horizontal");
+            rb2d.velocity = new Vector2(moveInput * speed, rb2d.velocity.y);
+
+        }
+
+    }
+}
---
+++ Assets/Destroy.cs
@@ -0,0 +1,84 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Destroy : MonoBehaviour
+{
+
+    public Text text;
+    public GameObject player;
+    public GameObject platformPrefab;
+    public GameObject springPrefab;
+    public GameObject myPlat;
+
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+
+    }
+
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+
+        if(collision.gameObject.name.StartsWith("Platform"))
+        {
+
+            if (Random.Range(1, 7) == 1)
+            {
+
+                Destroy(collision.gameObject);
+                Instantiate(springPrefab, new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f))), Quaternion.identity);
+
+
+            } else
+            {
+
+                collision.gameObject.transform.position = new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f)));
+
+            }
+
+        } else if(collision.gameObject.name.StartsWith("Spring"))
+        {
+
+            if (Random.Range(1, 7) == 1)
+            {
+
+                collision.gameObject.transform.position = new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f)));
+
+            }
+            else
+            {
+
+                Destroy(collision.gameObject);
+                Instantiate(platformPrefab, new Vector2(Random.Range(-4.5f, 4.5f), player.transform.position.y + (14 + Random.Range(0.2f, 1.0f))), Quaternion.identity);
+
+
+            }
+
+        }
+
+
+       // Destroy(collision.gameObject);
+
+
+       //  myPlat = (GameObject)Instantiate(platformPrefab, new Vector2(Random.Range(-5.5f, 5.5f), player.transform.position.y + (14 + Random.Range(0.5f, 0.8f))), Quaternion.identity);
+       //  Debug.Log("Created Normal");
+
+        //    if (Random.Range(1, 6) == 1)
+       //       {
+
+        //       Instantiate(springPrefab, new Vector2(myPlat.transform.position.x, myPlat.transform.position.y + 0.4f), Quaternion.identity);
+
+        //   }
+    }
+
+}
